14e7805903ee93d87991a881aa789a86e9f56b2e
Todd Lunter <tlunter@gmail.com>
2013-11-07 20:35:08 -0500

    Add vendor necessities

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-recess');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      react: {
+        files: [
+          {
+            expand: true,
+            cwd: 'bower_components/react/',
+            src: ['react.js','JSXTransformer.js'],
+            dest: 'lib/hn_history/public/js/'
+          }
+        ]
+      },
+      jquery: {
+        files: [
+          {
+            expand:true,
+            cwd: 'bower_components/jquery/',
+            src: ['jquery.js'],
+            dest: 'lib/hn_history/public/js/'
+          }
+        ]
+      },
+      assets: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/js/',
+            src: ['**/*.js'],
+            dest: 'lib/hn_history/public/js/'
+          }
+        ]
+      }
+    },
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      css: {
+        src: ['assets/less/**/*.less'],
+        dest: 'tmp/less/<%= pkg.name %>.less'
+      },
+    },
+    watch: {
+      all: {
+        files: ['<%= concat.js.src %>', '<%= concat.css.src %>'],
+        tasks: ['concat:js', 'concat:css', 'recess:dist', 'recess:min', 'timestamp']
+      }
+    },
+    recess: {
+      dist: {
+        options: {
+          compile: true
+        },
+        files: {
+          'lib/hn_history/public/css/<%= pkg.name %>.css': 'tmp/less/*.less'
+        }
+      },
+      min: {
+        options: {
+          compress: true
+        },
+        files: {
+          'lib/hn_history/public/css/<%= pkg.name %>.min.css': 'lib/hn_history/public/css/<%= pkg.name %>.css'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['copy', 'concat', 'recess']);
+
+  grunt.registerTask('timestamp', function() {
+    grunt.log.subhead(Date());
+  });
+};
+
