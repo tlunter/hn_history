c36ffddc5fbd6a0c5cee58e3a1e4734923994452
Todd Lunter <tlunter@gmail.com>
2014-03-08 21:16:08 +0000

    Add Oboe in production environment

--- lib/hn_history/app.rb
+++ lib/hn_history/app.rb
@@ -2,8 +2,21 @@ require 'hn_history'
 require 'hn_history/api'
 require 'hn_history/web'
 
+if ENV['RACK_ENV'] == "production"
+  require 'oboe'
+  require 'oboe/inst/rack'
+
+  Oboe::Config[:tracing_mode] = 'through'
+
+  Oboe::Ruby.initialize
+end
+
 def app
   Rack::Builder.app do
+    if ENV['RACK_ENV'] == "production"
+      use Oboe::Rack
+    end
+
     use Rack::Session::Cookie
     run Rack::Cascade.new [HnHistory::BaseAPI, HnHistory::Web]
   end
