dda8deb891ab654c8c070afa94e93e9318f517b8
Todd Lunter <tlunter@gmail.com>
2014-06-08 14:34:20 +0000

    Call method for job, easier to debug

--- lib/hn_history/jobs/photos.rb
+++ lib/hn_history/jobs/photos.rb
@@ -1,27 +1,35 @@
 module HnHistory
-  Clockwork.every(5.minutes, 'photos.new') do
-    current_time = Time.now.utc
+  module Jobs
+    class << self
+      def get_photo
+        current_time = Time.now.utc
 
-    puts "Photo for: #{current_time.xmlschema(3)}"
+        puts "Photo for: #{current_time.xmlschema(3)}"
 
-    entries = RubyHackernews::Entry.all.select(&:id)
+        entries = RubyHackernews::Entry.all.select(&:id)
 
-    return if entries.length == 0
+        return if entries.length == 0
 
-    photo = HnHistory::Models::Photo.new
-    photo.created_at = current_time.to_i
-    photo.save
+        photo = HnHistory::Models::Photo.new
+        photo.created_at = current_time.to_i
+        photo.save
 
-    entries.map do |e|
-      entry = HnHistory::Models::Entry.new
-      entry.entry_id = e.id
-      entry.title = e.link.title
-      entry.link = e.link.href
-      entry.site = e.link.site
-      entry.upvotes = e.voting.score
-      entry.position = e.number
-      entry.photo = photo
-      entry.save
+        entries.map do |e|
+          entry = HnHistory::Models::Entry.new
+          entry.entry_id = e.id
+          entry.title = e.link.title
+          entry.link = e.link.href
+          entry.site = e.link.site
+          entry.upvotes = e.voting.score
+          entry.position = e.number
+          entry.photo = photo
+          entry.save
+        end
+      end
     end
   end
+end
+
+Clockwork.every(5.minutes, 'photos.new') do
+  HnHistory::Jobs.get_photo
 end
