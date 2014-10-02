0ee6601af07a691143becc587c7046942ac619c6
Todd Lunter <tlunter@gmail.com>
2014-10-01 23:04:31 -0400

    Remove flickering unless button states are changing

--- assets/jsx/front_page.jsx
+++ assets/jsx/front_page.jsx
@@ -1,18 +1,40 @@
 /** @jsx React.DOM */
 var Views = Views || {};
 Views.FrontPage = React.createClass({
-  getInitialState: function() { return { items: [] } },
+  getInitialState: function() {
+    return { items: [], prevObj: {}, nextObj: {} }
+  },
   setItems: function() {
     var items = this.props.modelList.items;
     if (this.props.time) {
-      if (items.length != this.state.items.length) {
-        this.setState({ items: items });
+      console.log("Not auto-following");
+      var timeObj = _.find(items, function(i) { return i.created_at == this.props.time }.bind(this));
+      var timeIndex = _.indexOf(items, timeObj);
+      var prevObj = items[timeIndex - 1] || {};
+      var nextObj = items[timeIndex + 1] || {};
+
+      if (prevObj.created_at != this.state.prevObj.created_at || nextObj.created_at != this.state.nextObj.created_at) {
+        console.log("Resetting state, something updated");
+        this.setState({
+          items: items,
+          prevObj: prevObj,
+          nextObj: nextObj
+        });
       }
     } else {
+      console.log("Auto-following");
       if (items.length > 0) {
         var index = items.length - 1;
         if (this.state.time != items[index].created_at) {
-          this.setState({ items: items, time: items[index].created_at });
+          console.log("New photo to show");
+          console.log("Last time: " + this.state.time);
+          console.log("New time: " + items[index].created_at);
+          this.setState({
+            items: items,
+            time: items[index].created_at,
+            prevObj: items[index - 1],
+            nextObj: {}
+          });
         }
       }
     }
